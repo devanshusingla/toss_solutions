6a89c1d967c1d78b9ff8f54ebf12ee96dbb41cce
devanshu singla <56247443+devanshusingla@users.noreply.github.com>
2019-10-16 23:33:13 +0530

    Update direct_input_output.c
    input is taken in string str. Then integers are separated from the string in the first for loop and stored in array arr. Then members of arr are printed.

--- direct_input_output.c
+++ direct_input_output.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 
 int main(){
-    char *str = (char *)malloc(sizeof(char)) ;
-    int n=0, *arr = (int *)malloc(sizeof(int)) ;
-    unsigned long len = 1;
+    char *str = (char *)malloc(sizeof(char)) ;//stores initial input
+    int n=0, *arr = (int *)malloc(sizeof(int)) ;//n stores number of elements in arr. arr stores integers
+    unsigned long len = 1;//stores length of string
     *arr = 0 ;
     getline(&str, &len, stdin ) ;
-    for(int i=0; i<len; i++){
+    for(int i=0; i<len; i++){//separates integer input from string and stores in arr
         if((*(str+i)<='9')&&(*(str+i)>='0')){
             *(arr+n) = *(arr+n)*10 + (int)(*(str+i) - '0') ;
         }
@@ -16,7 +16,7 @@ int main(){
             arr = (int *)realloc((void *) arr,(++n+1)*sizeof(int)) ;
         }
     }
-    for(int i=0; i<=n; i++){
+    for(int i=0; i<=n; i++){//print the numbers separated by space
         printf("%d ",*(arr+i)) ;
     }
     return 0 ;
